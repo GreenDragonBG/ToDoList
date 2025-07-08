4b2340abd5b6b2fb4f97f5c05160f4420c904262
IvanK <ivan_koyrushki@abv.bg>
2025-07-08 10:37:43 +0200

    pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ToDoList/',
   server: {
     port: 3001,
     open: true
   },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     sourcemap: true
   }
-}) 
+})
